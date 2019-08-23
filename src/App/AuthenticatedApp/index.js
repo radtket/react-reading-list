d20933b5960362d6fdf959e61470f752597f2aad
Taylor Radtke <taylorradtke@gmail.com>
2019-08-23 18:51:39 -0400

    Authen

--- src/App/AuthenticatedApp/index.js
+++ src/App/AuthenticatedApp/index.js
@@ -1,62 +1,109 @@
 import React from "react";
+import { Link } from "@reach/router";
 
-/** @jsx jsx */
-import { jsx } from "@emotion/core";
+// Material UI
+import {
+  AppBar,
+  Button,
+  CssBaseline,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { FindInPage, LocalLibrary, PlaylistAddCheck } from "@material-ui/icons";
 
-import * as mq from "../../styles/media-queries";
+// Components
 import { useAuth } from "../../context/auth-context";
 import { useUser } from "../../context/user-context";
 import Routes from "./routes";
-import Nav from "./Nav";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
 const AuthenticatedApp = () => {
+  const classes = useStyles();
   const user = useUser();
   const { logout } = useAuth();
+
   return (
-    <div
-      css={{
-        margin: "0 auto",
-        padding: "2em 0",
-        maxWidth: "840px",
-        width: "100%",
-        display: "grid",
-        gridGap: "1em",
-        gridTemplateColumns: "3fr 9fr",
-        [mq.small]: {
-          gridTemplateColumns: "1fr",
-          gridTemplateRows: "auto",
-          width: "100%",
-          padding: "2em 1em",
-        },
-      }}
-    >
-      <div>
-        <div
-          css={{
-            display: "flex",
-            alignItems: "center",
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-          }}
-        >
-          {user.username}
-          <button
-            css={{
-              marginLeft: "10px",
-            }}
-            onClick={logout}
-            type="button"
-          >
-            Logout
-          </button>
-        </div>
-        <Nav />
-      </div>
-      <main css={{ width: "100%" }}>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar className={classes.appBar} position="fixed">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6">
+            {user.username}
+          </Typography>
+          <Button color="inherit" onClick={logout}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        className={classes.drawer}
+        variant="permanent"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button component={Link} to="/list">
+            <ListItemIcon>
+              <LocalLibrary />
+            </ListItemIcon>
+            <ListItemText primary="Reading List" />
+          </ListItem>
+          <ListItem button component={Link} to="/finished">
+            <ListItemIcon>
+              <PlaylistAddCheck />
+            </ListItemIcon>
+            <ListItemText primary="Finished Books" />
+          </ListItem>
+          <ListItem button component={Link} to="/discover">
+            <ListItemIcon>
+              <FindInPage />
+            </ListItemIcon>
+            <ListItemText primary="Discover" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes />
       </main>
-      <footer />
     </div>
   );
 };
